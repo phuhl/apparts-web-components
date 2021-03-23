87f33950f08b0cd55085dbbb5767ab8dc5fefc50
Philipp Uhl <git@ph-uhl.com>
2021-03-23 15:13:14 +0100

    Fixes classname for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Button = ({ submit, loading, disabled, ...props }) => {
+const Button = ({ submit, loading, disabled, className = "", ...props }) => {
   return (
     <button
       className={
-        (loading ? "loading " : "") + (submit ? "submit " : "") + "button"
+        (loading ? "loading " : "") +
+        (submit ? "submit " : "") +
+        "button " +
+        className
       }
       {...props}
       disabled={loading || disabled}
@@ -17,6 +20,7 @@ Button.propTypes = {
   loading: PropTypes.bool,
   disabled: PropTypes.bool,
   submit: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 export default Button;
